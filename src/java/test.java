f442e116a0abcdf6d0ebd56834588fdf7e1fbe18
wangdi18 <wangdi18@hikvision.com>
2020-06-24 18:46:10 +0800

    用户0提交代码2

--- src/java/test.java
+++ src/java/test.java
@@ -8,6 +8,7 @@ public class test {
         System.out.println("ninhao");
         System.out.println("git test");
         System.out.println("分支1");
-        System.out.println("用户0增加代码1");
+        System.out.println("用户0:增加代码1");
+        System.out.println("用户0:增加代码2")
     }
 }
