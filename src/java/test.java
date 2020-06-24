fe758c26b24129108319a7d5186e4ea71516cea0
wangdi18 <wangdi18@hikvision.com>
2020-06-24 17:04:14 +0800

    分支1提交

--- src/java/test.java
+++ src/java/test.java
@@ -7,5 +7,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println("ninhao");
         System.out.println("git test");
+        System.out.println("分支1");
     }
 }
