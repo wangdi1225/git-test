1d9e53fb5f40ce15008636273bbdb950008e654e
wangdi18 <wangdi18@hikvision.com>
2020-06-24 16:58:24 +0800

    增加test

--- src/java/test.java
+++ src/java/test.java
@@ -1,7 +1,11 @@
 package java;
 
+
+//this is test
+//git test
 public class test {
     public static void main(String[] args) {
         System.out.println("ninhao");
+        System.out.println("git test");
     }
 }
