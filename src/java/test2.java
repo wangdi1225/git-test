438a4a64ab02e2a55b14a918790f51c358efa90e
wangdi18 <wangdi18@hikvision.com>
2020-07-24 16:29:07 +0800

    增加test2

---
+++ src/java/test2.java
@@ -0,0 +1,15 @@
+﻿package java;
+
+
+//this is test
+//git test
+public class test {
+    public static void main(String[] args) {
+        System.out.println("用户0:增加代码1");
+        System.out.println("用户0增加代码1");
+        System.out.println("用户1：添加代码1");
+        System.out.println("用户1:添加代码2");
+        System.out.println("用户0:增加代码2")
+		System.out.println("用户1:添加代码3");
+    }
+}
